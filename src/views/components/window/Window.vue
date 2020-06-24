bef5111d505cffa4c6d56c701216d49f3d848318
Son Bui Ngoc <buingocson1410@gmail.com>
2020-06-24 07:13:40 +0700

    update

---
+++ src/views/components/window/Window.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="window">
+        <div class="window__controller">
+            <div class="mini" id="btn-mini"><span>–</span></div>
+            <div class="full" id="btn-max"><span>❐</span></div>
+            <div class="close" id="btn-close"><span>✕</span></div>
+        </div>
+        <div class="window__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name     : 'Window',
+    components: {
+    },
+    computed : {
+    },
+    methods : {
+    },
+}
+</script>

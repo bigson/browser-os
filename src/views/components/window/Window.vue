37b0dd375abb572e90e5a00678b2aa6e23a397af
Son Bui Ngoc <buingocson1410@gmail.com>
2020-07-07 00:34:29 +0700

    update

--- src/views/components/window/Window.vue
+++ src/views/components/window/Window.vue
@@ -32,6 +32,8 @@
         </div>
         <div class="window__content">
             <slot></slot>
+            <img :src="s" width="200" height="200" />
+            <canvas id="canvas"></canvas>
         </div>
     </div>
 </template>
@@ -74,7 +76,8 @@ export default {
                 RESIZE_BOTTOM       : 'RESIZE_BOTTOM',
                 RESIZE_LEFT_BOTTOM  : 'RESIZE_LEFT_BOTTOM',
                 RESIZE_RIGHT_BOTTOM : 'RESIZE_RIGHT_BOTTOM',
-            }
+            },
+            s : '/img/b.svg',
         }
     },
     components: {
@@ -166,46 +169,116 @@ export default {
             }
         },
 
-        captionBackground(){
+        async captionBackground(){
             let
-                canvas = document.createElement('canvas'),
-                ctx    = canvas.getContext('2d'),
+                // canvas = document.createElement('canvas'),
+                // canvas  = document.getElementById('canvas'),
+                // ctx     = canvas.getContext('2d'),
                 desktop = document.querySelector('.desktop'),
+                styles  = document.querySelectorAll('style'),
+                html    = desktop.innerHTML.replace(/<img [^>]+/gi, '$&/'),
+                regex   = RegExp(/url\(("|')([^"']+)("|')\)/, 'gi'),
+                regexImg = RegExp(/<img([^>]*)(src=("|')([^"']+)("|'))/, 'gi'),
+                urls    = {},
+                promiseAll = [],
+                matchStyle,
+                matchHtml
 
-                styles = document.querySelectorAll('style'),
-                data   =
-                `<svg xmlns="http://www.w3.org/2000/svg" width="${desktop.offsetWidth}" height="${desktop.offsetHeight}">
-                    <g class="foreignObjectWrapper">
-                    <foreignObject width="${desktop.offsetWidth}" height="${desktop.offsetHeight}">
-                        <div xmlns="http://www.w3.org/1999/xhtml" style="width:${desktop.offsetWidth}px;height:${desktop.offsetHeight}px">
-                            ${Object.values(styles).map(x => x.outerHTML).join('').replace(/url\((['|"])/ig, '$&http://localhost:8080')}
-                            <div class="desktop" style="width:${desktop.offsetWidth}px;height:${desktop.offsetHeight}px">
-                                ${desktop.innerHTML}
-                            </div>
-                        </div>
-                    </foreignObject>
-                    </g>
-                </svg>`
+                styles = Object.values(styles).map(x => x.innerHTML).join('')
 
-                var DOMURL = window.URL || window.webkitURL || window;
+                matchStyle = styles.matchAll(regex)
+                matchHtml  = html.matchAll(regexImg)
 
-                var img = new Image();
-                var svg = new Blob([data], {
-                  type: 'image/svg+xml;charset=utf-8'
-                });
-                var url = DOMURL.createObjectURL(svg);
-
-                console.log( url)
-
-                img.onload = function() {
-                  ctx.drawImage(img, 0, 0);
-
-
-                    var img    = canvas.toDataURL("image/png");
-                  DOMURL.revokeObjectURL(url);
+                for(let i of matchStyle){
+                    if(!urls[i[2]]){
+                        urls[i[2]] = {url : i[2], data : ''}
+                    }
+                }
+                for(let i of matchHtml){
+                    if(!urls[i[4]]){
+                        urls[i[4]] = {url : i[4], data : ''}
+                    }
+                }
+                console.log(urls)
+                // convert image url → base64
+                for(let i in urls){
+                    promiseAll.push(new Promise(() => {
+                        return this.getImageFromUrl(i)
+                    }).then(base64 => urls[i].data = base64))
                 }
 
+                await Promise.all(promiseAll)
+
+                console.log(urls)
+                return
+
+                // styles =
+
+
+
+                // .join('').replace(/url\((['|"])/ig, '$&http://localhost:8080')
+
+                // let data   = `
+                // <svg xmlns="http://www.w3.org/2000/svg" width="${desktop.offsetWidth}" height="${desktop.offsetHeight}">
+                //     <g class="foreignObjectWrapper">
+                //     <foreignObject width="${desktop.offsetWidth}" height="${desktop.offsetHeight}" style="transform: translate(0, 0);">
+                //         <div class="desktop" xmlns="http://www.w3.org/1999/xhtml" style="width:${desktop.offsetWidth}px;height:${desktop.offsetHeight}px;">
+                //             ${styles}
+                //             ${html}
+
+                //         </div>
+                //     </foreignObject>
+                //     </g>
+                // </svg>`
+
+                // var DOMURL = window.URL || window.webkitURL || window;
+
+                // var img = new Image();
+                // var svg = new Blob([data], {
+                //   type: 'image/svg+xml;charset=utf-8'
+                // });
+                // var url = DOMURL.createObjectURL(svg);
+
+                // console.log( url)
+                // // this.s = url
+
+                // img.onload = function() {
+                //   ctx.drawImage(img, 0, 0);
+
+
+                //     var img    = canvas.toDataURL("image/png");
+                //   DOMURL.revokeObjectURL(url);
+                // }
+
                 // img.src = url;
+        },
+
+        async getImageFromUrl(url){
+            console.log(url)
+
+            if(!url.startsWith('//')){
+                url = location.protocol + url
+            }else if(url.startsWith('/')){
+                url = location.origin + url
+            }
+
+            return await fetch(url)
+                .then(response => response.arrayBuffer())
+                .then(buffer => {
+                    let base64Flag = 'data:image/jpeg;base64,',
+                        imageStr = this.arrayBufferToBase64(buffer)
+
+                    return (base64Flag + imageStr)
+                })
+        },
+
+        arrayBufferToBase64(buffer) {
+            let binary = '',
+                bytes  = [].slice.call(new Uint8Array(buffer))
+
+            bytes.forEach((b) => binary += String.fromCharCode(b));
+
+            return window.btoa(binary);
         }
     },
 }

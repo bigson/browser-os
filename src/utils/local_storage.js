cf05d0e07a2385c63139c19950194a888b6076fc
Son Bui Ngoc <buingocson1410@gmail.com>
2020-11-03 06:51:47 +0700

    a

--- src/utils/local_storage.js
+++ src/utils/local_storage.js
@@ -1,14 +1,14 @@
 export function getItem(key, value){
     let myStorage = window.localStorage
-    return localStorage.getItem(key)
+    return myStorage.getItem(key)
 }
 
 export function setItem(key, value){
     let myStorage = window.localStorage
-    localStorage.setItem(key, value)
+    myStorage.setItem(key, value)
 }
 
 export function clearAll(){
-	myStorage = window.localStorage
-    localStorage.clear()
+	let myStorage = window.localStorage
+    myStorage.clear()
 }
--- src/utils/session_storage.js
+++ src/utils/session_storage.js
@@ -1,19 +1,19 @@
 export function getItem(key, value){
-    sessionStorage = window.sessionStorage;
+    let sessionStorage = window.sessionStorage;
     return sessionStorage.getItem(key)
 }
 
 export function setItem(key, value){
-    let myStorage = window.localStorage
+    let sessionStorage = window.localStorage
     sessionStorage.setItem(key, value)
 }
 
 export function removeItem(key){
-    let myStorage = window.localStorage
+    let sessionStorage = window.localStorage
     sessionStorage.removeItem(key)
 }
 
 export function clearAll(){
-	myStorage = window.localStorage
+	let sessionStorage = window.localStorage
     sessionStorage.clear()
 }
